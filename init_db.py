374946067c45a993e73790a2ce00f6c434c1f8b6
harukiinharu <harukiinharu@outlook.com>
2023-02-26 17:23:38 +0800

    change name to "春木树洞"

--- init_db.py
+++ init_db.py
@@ -12,6 +12,6 @@ CREATE TABLE posts (
 );'''
 conn.executescript(create_table)
 conn.execute("INSERT INTO posts (title, author, content) VALUES (?, ?, ?)",
-            ('第一篇博客', '春木', '你好，我是春木\n春风绿地树先知，欢迎来到我的博客\n'))
+            ('第一篇博客', '春木', '你好，我是春木\n春风绿地树先知，欢迎来到春木树洞\n'))
 conn.commit()
 conn.close()
